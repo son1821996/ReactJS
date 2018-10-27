3e70cf61b9a46f19f62eb9c996fcbae2dda5c7e0
nguyenson18296 <nguyenson18296@gmail.com>
2018-10-28 01:33:37 +0700

    JSX

--- JSX/jsx/src/App.js
+++ JSX/jsx/src/App.js
@@ -3,10 +3,61 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  showInfo(object){
+    return <h3>
+              <div className="object"> 
+              Ten: { object.id } <br />
+              Cong ty: { object.company} <br/>
+            </div>
+            </h3>
+  }
+
   render() {
+    const a = 5, b = 6;
+    const name = 'Nguyen Hoang Son';
+    const object = {
+      id: 1,
+      name: 'Nguyen van A',
+      company: 'Bakery',
+      avatar: ''
+    }
+
+    const users = [
+      {
+        id: 1,
+        name: 'Nguyen van A',
+        age: '18'
+      },
+      {
+        id: 2,
+        name: 'Nguyen van B',
+        age: '24'
+      },
+      {
+        id: 3,
+        name: 'Nguyen van C',
+        age: '12'
+      }
+    ]
+
+    const elements = users.map((user, index) => {
+      return <div key={ user.id }>
+                <h2>Ten: {user.name} </h2>
+                <h3>Tuoi : {user.age} </h3>
+              </div>
+    }); 
+
+
     return (
-      <div className="App">
+      <div>
+        <div className="number"> a + b =  { a + b }</div>
+        <div className="name">Ten : { name }</div>
+        { this.showInfo(object) }
+        <br/>
+        { elements }
       </div>
+        
     );
   }
 }
