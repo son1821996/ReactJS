4a200d277f870b14ed2ddf1e2b0569c70c409693
nguyenson18296 <nguyenson18296@gmail.com>
2018-10-28 22:21:51 +0700

    Lifting State Up

---
+++ 10-lifting-state-up/src/components/BoilingVerdict.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+export default BoilingVerdict
